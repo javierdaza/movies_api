6677918462086d0411a31f59ecddde9c746169e9
Javier Daza <javierdazacomas@gmail.com>
2020-06-20 17:31:21 -0500

    task iterates over 10 pages and creates movies

--- movies_api/movies/tasks.py
+++ movies_api/movies/tasks.py
@@ -9,24 +9,40 @@ env = environ.Env()
 
 @celery_app.task()
 def create_movies_database_task():
-    """Create movies database with new movies."""
+    """
+        Create movies database with new movies. Returns number of
+        newly created movies
+    """
     THE_MOVIE_API_KEY = env("THE_MOVIE_API_KEY")
-    resp = requests.get(
-        'https://api.themoviedb.org/3/movie/now_playing?api_key='+
-        THE_MOVIE_API_KEY+
-        '&language=en-US&page=1'
-    )
-    movies_array = []
-    response_size = len(resp.json()['results'])
-    for item in resp.json()['results']:
-        # TODO: Check if movie already exists
-        movies_array.append(
-            Movies(
-                title = item['title'],
-                description = item['overview'],
-                poster_path = item['poster_path'],
-                ranking = int(item['vote_average']),
-            )
+    count = 0
+    for page in range(1, 11):
+        resp = requests.get(
+            'https://api.themoviedb.org/3/movie/now_playing?api_key='+
+            THE_MOVIE_API_KEY+
+            '&language=en-US&page='+
+            str(page)
         )
-    Movies.objects.bulk_create(movies_array, response_size)
-    return resp.status_code
+        movies_array = []
+        response_size = len(resp.json()['results'])
+        tmdb_path = 'https://image.tmdb.org/t/p/w500'
+        for item in resp.json()['results']:
+            try:
+                movie = Movies.objects.get(tmdb_id=int(item['id']))
+            except Movies.DoesNotExist:
+                if item['poster_path']:
+                    poster_path = tmdb_path + item['poster_path']
+                else:
+                    poster_path = None
+
+                movies_array.append(
+                    Movies(
+                        title = item['title'],
+                        description = item['overview'],
+                        poster_path = poster_path,
+                        ranking = int(item['vote_average']),
+                        tmdb_id = int(item['id']),
+                    )
+                )
+                count += 1
+        Movies.objects.bulk_create(movies_array, response_size)
+    return count
